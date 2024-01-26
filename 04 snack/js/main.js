7493bf46582c773d9887a5841c85d42bfa8431db
bonvi83 <bonvi83@gmail.com>
2024-01-26 18:17:08 +0100

    qualche difficoltà sul 4

---
+++ 04 snack/js/main.js
@@ -0,0 +1,69 @@
+const teams = [
+    {
+        name: 'Team Turtle',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Frog',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Penguin',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Hippopotamus',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Seal',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Crocodile',
+        score: 0,
+        foul: 0,
+    },
+    {
+        name: 'Team Dolphin',
+        score: 0,
+        foul: 0,
+    },
+];
+
+teams.forEach ((team) => {
+    team.score = randomNumbers(1, 100);
+    team.foul = randomNumbers(1, 10);
+});
+
+
+function randomNumbers(min, max) {
+    let numRand = Math.floor(Math.random() * (max - min + 1) + min);
+    return numRand;
+};
+
+console.log(teams);
+
+
+
+
+// Creare un nuovo array i cui elementi contengono nomi e falli subiti, recuperarli tramite il destructuring, e console.log
+
+
+const newArray = teams.map((team) => {
+    const {name, foul} = team;
+    console.log(name, foul);
+
+    let newTeam = {
+        name: name,
+        foul: foul,
+    };
+    return newTeam;
+});
+
+console.log(newArray);
